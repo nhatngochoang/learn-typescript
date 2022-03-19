3d4a5282f11766641407a36556ce3f0133cd0ad7
tahn-0102 <ongthoxunu@gmail.com>
2022-03-20 00:07:23 +0700

    Typescript: 03-06 Một số Utility Types phổ biến 🎉

--- src/lectures/04-main.ts
+++ src/lectures/04-main.ts
@@ -9,3 +9,70 @@ export function isPositive(n:number):boolean{
 export function createStudent(student: {id:number, name:string, age:number}){
    const {id, name, age} = student
 }
+
+
+interface Student {
+   id: number;
+   name: string;
+}
+
+// Get all student keys ➤ literal type
+type StudentKeys = keyof Student;
+const keyList: StudentKeys = 'id'
+
+// 1 arg
+interface List<Type>{
+   length: number;
+   [index: number]: Type
+}
+// 2 args
+interface List2<T, K>{
+   id: K;
+   length: number;
+   [index: number]: T;
+}
+interface List3<T>{
+   length: number;
+   [index: string]: T | number; // index type must cover 
+}
+
+const studentList: Array<Student> = [
+   { id: 1, name: 'Alice'},
+   { id: 2, name: 'Bob'}
+]
+
+const studentList2: List<Student> = [
+   { id: 1, name: 'Alice'},
+   { id: 2, name: 'Bob'}
+]
+
+const numberList: Array<number> = [1, 2, 3];
+const numberList2: List<number> = [1,2,3]
+
+const wordList: Array<string> = ['easy', 'frontend'];
+const wordList2: List<string> = ['easy', 'frontend'];
+      
+// Mapped Types
+type OptionsFlags<T> = {
+   [Property in keyof T]: boolean
+}
+
+type FeatureFlags = {
+   darkMode: () => void;
+   newUserProfile: () => void
+}
+
+type FeatureOptions = OptionsFlags<FeatureFlags>
+
+// Complex Map
+type Getters<T> ={
+   [Property in keyof T as `get${Capitalize<string & Property>}`]:()=> T[Property]
+}
+
+interface Person {
+   name: string;
+   age: number;
+   location: string;
+}
+
+type LazyPerson = Getters<Person>

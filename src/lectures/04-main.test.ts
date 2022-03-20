e486ef818048c29f2a59b0f4c036a4b0b81686e9
tahn-0102 <ongthoxunu@gmail.com>
2022-03-20 16:55:27 +0700

    TypeScript cơ bản - Tập 4: Generics

--- src/lectures/04-main.test.ts
+++ src/lectures/04-main.test.ts
@@ -1,22 +1,19 @@
-import {isPositive} from'./04-main'
+import { isPositive } from './04-main';
 
 describe('isPositive()', () => {
-   it('should return true when n > 0',() =>{
-         expect(isPositive(1) === true);
-         expect(isPositive(2) === true);
-         expect(isPositive(3) === true);
-      });
+  it('should return true when n > 0', () => {
+    expect(isPositive(1) === true);
+    expect(isPositive(2) === true);
+    expect(isPositive(3) === true);
+  });
 
-      
-      it('should return false when n = 0',() =>{
-            expect(isPositive(0) === false);
-         });
+  it('should return false when n = 0', () => {
+    expect(isPositive(0) === false);
+  });
 
-         it('should return false when n <0',() =>{
-               expect(isPositive(-1).toBe(false));
-               expect(isPositive(-2) === false);
-               expect(isPositive(-5) === false);
-            })
-            
-})
-
+  it('should return false when n <0', () => {
+    expect(isPositive(-1).toBe(false));
+    expect(isPositive(-2) === false);
+    expect(isPositive(-5) === false);
+  });
+});

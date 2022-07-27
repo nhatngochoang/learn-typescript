e201a19f3ab2bd17cd210cc933352fc871f62d70
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-07-27 17:51:24 +0700

    Update Generics.ts

--- src/lectures/Generics/Generics.ts
+++ src/lectures/Generics/Generics.ts
@@ -1,56 +1,77 @@
-// Class
-class KeyValuePair<K, V> {
-   constructor(public key: K, public value: V) {}
+// Array
+interface TArray<T> {
+  length: number;
+  [index: number]: T;
 }
 
-let pair = new KeyValuePair(1, 'a')
+const numbersList: TArray<number> = [1, 2, 3];
+interface Student {
+  id: number;
+  name: string;
+}
+
+const studentsList: TArray<Student> = [{ id: 1, name: "John" }];
+
+// Object
+interface TObject<T> {
+  length: number;
+  isAdj: boolean;
+  [index: string]: T | number | boolean;
+}
+
+// Class
+class KeyValuePair<K, V> {
+  constructor(public key: K, public value: V) {}
+}
+
+let pair = new KeyValuePair(1, "a");
 
 // Function
 class ArrayUtils {
-   static wrapInArray<T>(value: T) {
-      return [value]
-   }
+  static wrapInArray<T>(value: T) {
+    return [value];
+  }
 }
 
-let numbers = ArrayUtils.wrapInArray(1)
+let numbers = ArrayUtils.wrapInArray(1);
 
 // Interface
 interface Result<T> {
-   data: T | null
-   error: string | null
+  data: T | null;
+  error: string | null;
 }
 
 function fetch<T>(url: string): Result<T> {
-   return { data: null, error: null }
+  return { data: null, error: null };
 }
 
 interface User {
-   username: string
+  username: string;
 }
 
 interface Product {
-   title: string
+  title: string;
 }
 
-let result = fetch<User>('url')
+let result = fetch<User>("url");
 
-result.data?.username
+result.data?.username;
 
 // Constraints
 interface User {
-   name: string
+  name: string;
 }
 
 function echo<T extends number | string | User>(value: T): T {
-   return value
+  return value;
 }
 
 // Constraints Class
 class User {
-   constructor(public name: string) {}
+  constructor(public name: string) {}
 }
 
 class Admin extends User {}
 
-echo(new User('a'))
-echo(new Admin('a'))
+echo(new User("a"));
+echo(new Admin("a"));

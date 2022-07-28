36a89bb1ef8d8b3da0840a04a7481036bef66418
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-07-29 02:29:57 +0700

    Create as.ts

---
+++ src/lectures/keywords/as.ts
@@ -0,0 +1,6 @@
+let input1 = document.querySelector('input[type="text"]') as HTMLInputElement;
+
+// Ép kiểu bằng as hoặc <>
+let input2 = <HTMLInputElement>document.querySelector('input[type="text"]');
+
+// Ưu tiên dùng as trong React

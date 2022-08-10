48a6b428b15cf295350d8a95fe7f44ac0859ab0b
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-08-10 13:45:25 +0700

    Improving OBJECT.KEYS in TypeScript
    https://www.youtube.com/watch?v=GW00zebIt0g&list=PLIvujZeVDLMx040-j1W4WFs1BxuTGdI_b&index=11&ab_channel=MattPocock

--- src/AdvancedTypeScript.ts
+++ src/AdvancedTypeScript.ts
@@ -1,6 +1,17 @@
-// REMOVE a member of a Union Type
+// 1.REMOVE a member of a Union Type
 type Letters = "a" | "b" | "c";
 
 type RemoveC<T> = T extends "c" ? never : T;
 
 type WithoutC = RemoveC<Letters>;
+
+// 2.Improving OBJECT.KEYS in TypeScript
+const myObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+const objectKeys = <Obj>(obj: Obj) => Object.keys(obj) as Array<keyof Obj>;
+
+objectKeys(myObject).forEach((key) => console.log(myObject[key]));

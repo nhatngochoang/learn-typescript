f538b68684fe2a2818c4368507123d6883532e4b
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-09-03 00:56:56 +0700

    feat: update package, path

--- src/lectures/04-main.test.ts
+++ src/lectures/04-main.test.ts
@@ -1,18 +1,18 @@
-import { isPositive } from './04-main';
+import { isPositive } from "./04-main";
 
-describe('isPositive()', () => {
-  it('should return true when n > 0', () => {
+describe("isPositive()", () => {
+  it("should return true when n > 0", () => {
     expect(isPositive(1) === true);
     expect(isPositive(2) === true);
     expect(isPositive(3) === true);
   });
 
-  it('should return false when n = 0', () => {
+  it("should return false when n = 0", () => {
     expect(isPositive(0) === false);
   });
 
-  it('should return false when n <0', () => {
-    expect(isPositive(-1).toBe(false));
+  it("should return false when n <0", () => {
+    expect(isPositive(-1) === false);
     expect(isPositive(-2) === false);
     expect(isPositive(-5) === false);
   });
